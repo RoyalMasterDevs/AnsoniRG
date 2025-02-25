857289cd9e534e1a5546820a0d1df0f92ea7dd6e
ANSONI RAMIREZ GARCIA <ansoniramirezgarcia4@gmail.com>
2025-02-24 20:38:18 -0600

    Add files via upload
    Twosum

---
+++ twosum.py
@@ -0,0 +1,9 @@
+numDosDigitos = input("ingresa número entero conformado por dos digitos: ")
+
+
+primerdigito = numDosDigitos[0]
+segundoDigito = numDosDigitos[1]
+
+
+resultado = int(primerdigito) + int(segundoDigito)
+print("El resultado de la suma de los digitos es: " + str(resultado))
